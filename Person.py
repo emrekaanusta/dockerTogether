41fb967af60886d72f25515285c87e0934795693
gorkemmayadag <gorkemmert.mayadag@bahcesehir.edu.tr>
2023-07-12 16:40:41 +0300

    made functions for each CRUD application

--- Person.py
+++ Person.py
@@ -1,87 +1,126 @@
 import json
 from pymongo import MongoClient
 
+class myStruct():
+    def __init__(self,name,admin,project,username):
+        self.name=name
+        self.admin=admin
+        self.project=project
+        self.username=username
+
+def read_person_list():
+    mongoList = people.find({},{"_id":0})
+    for person in mongoList:
+            print(person)
+
+def create_person(name_input, admin_input, project_input, username_input):
+    if admin_input.lower() == "yes":
+        admin_input_int = 1
+    else:
+        admin_input_int = 0    
+    person_string = '{"name": "%s", "isAdmin": %d, "project": "%s", "username": "%s"}' % (name_input,admin_input_int,project_input,username_input)
+    new_person = json.loads(person_string)
+    #Inserting the new entry to the collection
+    db.people.insert_one(new_person)
+
+    print("New Person added to database\n %s" % db.people.find_one({"username": username_input}, {"_id": 0}))
+
+def update_person(username_to_updatef, updated_name, updated_admin, updated_project, updated_username):
+
+    if updated_admin.lower() == "yes":
+        updated_admin_int = 1
+    else:
+        updated_admin_int = 0
+
+    for userN in people.find(username_to_updatef):
+        
+        if updated_name != "":
+            db.people.update_one(username_to_updatef, {"$set": {"name": updated_name}})
+        if updated_admin != "":
+            db.people.update_one(username_to_updatef, {"$set": {"isAdmin": updated_admin_int}})
+        if updated_project != "":
+            db.people.update_one(username_to_updatef, {"$set": {"project": updated_project}})
+        if updated_username != "":
+            db.people.update_one(username_to_updatef, {"$set": {"username": updated_username}})
+    
+    if updated_name == username_to_updatef or updated_name == "":
+        print("Person updated\n %s" % db.people.find_one(username_to_updatef, {"_id": 0}))
+    else:
+        print("Person updated\n %s" % db.people.find_one({"username": updated_username}, {"_id": 0}))
+            
+def delete_person(user_to_delete, person_to_delete):
+        
+        #Little security question
+        answer = input("Are you sure you want to delete the following person?\n %s\n" % db.people.find_one({"username": person_to_delete}, {"_id": 0}))
+
+        if answer.lower() == "yes":
+            print("Following person is deleted from the database\n %s" % db.people.find_one({"username": person_to_delete}, {"_id": 0}))
+            db.people.delete_one(user_to_delete)
+        else:
+            print("Deletion process is cancelled")
 
 #CREATE READ UPDATE DELETE
 
-#Connection to client
-client = MongoClient("localhost", 27017)
+if __name__ == "__main__":
+    #Connection to client
+    client = MongoClient("localhost", 27017)
 
-#Creating database in the client
-db = client["nokiaProject"]
+    #Creating database in the client
+    db = client["nokiaProject"]
 
-people = db["people"]
+    people = db["people"]
 
-'''
+#dosya varsa yüklemesin
 #LOADING JSON FILE
-with open('people.json') as file:
-    file_data = json.load(file)
+#with open('people.json') as file:
+#    file_data = json.load(file)
+#people.insert_many(file_data)
 
-people.insert_many(file_data)
-'''
-
-#Find all documents and exclude their ObjectId
-x = people.find({},{"_id":0})
-
-
+#fonksiyonları teker teker böl
 while True: #Does not exit the code until you give command to exit
 
-    selection = int(input("\nWhat would you like to do with Person database? \n 1 to read \n 2 to write \n 3 to update \n 4 to delete\n 5 to exit\n"))
+    selection = input("\nWhat would you like to do with Person database?\n Read\n Write\n Update\n Delete\n Exit\n")
 
-    if selection == 1: #Printing everything in people collection
+    if selection.lower() == "read": #Printing everything in people collection
         
-        for person in x:
-            print(person)
+        read_person_list()
+        
 
-    elif selection == 2: #Adding a new entry to the collection
-
+    elif selection.lower() == "write": #Adding a new entry to the collection PERSON 
+        
         #Taking user input for the fields of the new entry
-        nameInput = str(input("Name: "))
-        adminInput = float(input("Is this and admin? 0 for 'No', 1 for 'Yes' "))
-        projectInput = str(input("Project: "))
-        idInput = int(input("User ID: "))
-    
-        #Inserting the new entry to the collection
-        db.people.insert_one({"name": nameInput, "isAdmin": adminInput, "project": projectInput, "id": idInput})
+        nameInput = input("Name: ")
+        adminInput = input("Is this an admin? Yes or No ")
+        projectInput = input("Project: ")
+        usernameInput = input("Username: ")
 
-        print("New Person added to database\n %s" % db.people.find_one({"id": idInput}, {"_id": 0}))
+        create_person(nameInput, adminInput, projectInput, usernameInput)
         
-    elif selection == 3: #Updates an already existing entry
+    elif selection.lower() == "update": #Updates an already existing entry
         #Identifying the entry to be updated
-        person_to_update = int(input("Please enter the ID of the person to be updated: "))
-        filter1 = {"id": person_to_update}
+        person_to_update = input("Please enter username of the person to be updated: ")
+        username_to_update = {"username": person_to_update}
     
         #Printing the entry's current values for clarity
-        print(db.people.find_one({"id": person_to_update}, {"_id": 0}))
+        print(db.people.find_one(username_to_update, {"_id": 0}))
     
         #Taking the soon-to-be-updated values for the entry
-        updated_name = str(input("Please enter a new name (Enter the current one if you wish to keep it): "))
-        updated_admin = float(input("Please enter the new admin permissions (Enter the current one if you wish to keep it): "))
-        updated_project = str(input("Please enter a new project name (Enter the current one if you wish to keep it): "))
-        updated_id = int(input("Please enter a new ID number (Enter the current one if you wish to keep it): "))
-        new_values = {"$set":{"name": updated_name, "isAdmin": updated_admin, "project": updated_project, "id": updated_id}}
+        updatedName = input("Please enter a new name (Leave blank if you wish to keep it): ")
+        updatedAdmin = input("Please enter the new admin permissions, yes or no (Leave blank if you wish to keep it): ")
+        updatedProject = input("Please enter a new project name (Leave blank if you wish to keep it): ")
+        updatedUsername = input("Please enter a new username (Leave blank if you wish to keep it): ")
 
-        #Actually making the update
-        db.people.update_one(filter1, new_values)
+        update_person(username_to_update, updatedName, updatedAdmin, updatedProject, updatedUsername)
 
-        print("Person updated\n %s" % db.people.find_one({"id": updated_id}, {"_id": 0}))
-        
-
-    elif selection == 4: #Deletes an existing entry
+    elif selection.lower() == "delete": #Deletes an existing entry
         #Identifying the entry to be deleted
-        person_to_delete = int(input("Please enter the ID of the person to be deleted: "))
-        filter2 = {"id": person_to_delete}
+        person_to_delete = input("Please enter the username of the person to be deleted: ")
+        username_to_delete = {"username": person_to_delete}
+        
+        delete_person(username_to_delete, person_to_delete)
 
-        #Little security question
-        delete_answer = str(input("Are you sure you want to delete the following person?\n %s\n" % db.people.find_one({"id": person_to_delete}, {"_id": 0})))
-
-
-        if delete_answer.lower() == "yes":
-            print("Following person is deleted from the database\n %s" % db.people.find_one({"id": person_to_delete}, {"_id": 0}))
-            db.people.delete_one(filter2)
-        else:
-            print("Deletion process is cancelled")
-            continue
-    else:
+    elif selection.lower() == "exit":
         break
+    else:
+        print("Please enter a valid command")
 

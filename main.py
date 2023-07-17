2570fc5f7ca305ad437139cd343e3fe0f927cb9b
gizemico <gizemseval48@gmail.com>
2023-07-17 16:00:36 +0300

    main script is updated

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+
+from pymongo import MongoClient
+from bson import ObjectId
+import json
+import os
+
+import db.db_manager
+from services.project_service import fileOperation,createProject,menu,readProject,updateProject,deleteProject
+from db.db_manager import Database 
+
+database = Database()
+database.connect_database()
+project_collection = database.project_collection
+
+
 def main():
     print("------Welcome to 'DockerTogether' App------\n")
     print("Please select one of the option:\n" 
@@ -14,9 +29,49 @@ def main():
         if option == 1:
             with open("Person.py") as f1:
                 exec(f1.read())
+                
         elif option == 2:
-            with open("project.py") as f2:
-                exec(f2.read())
+            
+            # Zero is used for load datas from json file to mongoDB, it is optional
+            zero_option = int(input("Please enter 0 in order to adding datas from JSON file to MongoDB:  "))
+            if zero_option == 0:
+                filepath = input("Enter the JSON file path: ")
+                fileOperation(filepath) # function call
+
+
+            menu() #function call
+            selection = int(input())
+
+            if selection == 5:
+                print("Good Bye!\n")
+                exit(0)
+
+            while selection != 5:
+                if selection == 1:
+                    name = str(input("\nEnter name: "))
+                    desc = str(input("\nEnter description: "))
+                    project = str(input("\nEnter project: "))
+                    createProject(name, desc, project)
+                elif selection == 2:
+                    readProject()
+                elif selection == 3:
+                    updated_project = str(input("Enter the unique object ID of the information you want to update: "))
+                    uid = ObjectId(updated_project)
+                    name = str(input("\nEnter name: "))
+                    desc = str(input("\nEnter description: "))
+                    project = str(input("\nEnter project: "))
+                    updateProject(uid, name, desc, project)
+                elif selection == 4:
+                    deleted_project = str(input("Enter the unique object ID you want to delete: "))
+                    uid = ObjectId(deleted_project)
+                    deleteProject(uid)
+                else:
+                    print("Good Bye!\n")
+                    exit(0)
+                menu()
+                selection = int(input())
+
+
         elif option == 3:
             with open("device.py") as f3:
                 exec(f3.read())

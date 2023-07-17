d8bdbe913a0e48a55fc34b393831487158da164d
gizemico <gizemseval48@gmail.com>
2023-07-17 16:03:51 +0300

    db_manager secon version

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,14 +1,34 @@
 from pymongo import MongoClient
 
-class database:
-    def connect_database():
+
+
+class Database:
+
+    def connect_database(self):
+        
+        
+        '''
         # connecting MongoDB instance
-        client = MongoClient("localhost", 27017)
+        #client = MongoClient("localhost", 27017)
 
         #  If the 'projectdb' doesn't exist, it is created. Otherwise, it is used.
         db = client.main_db
+        client = MongoClient("mongodb://localhost:27017/")
+        db.client["projectdb"]
+        
+        # If 'project' collection doesn't exist, it will be created. Otherwise, it is used.
+        #device_collection = db.collection
+        #person_collection = db.collection
+        self.project_collection = db["projects"]
+        '''
+
+        # connecting MongoDB instance
+        client = MongoClient("localhost", 27017)
+        
+
+        #  If the 'projectdb' doesn't exist, it is created. Otherwise, it is used.
+        db = client.main_db
+        client = MongoClient("mongodb://localhost:27017/")
 
         # If 'project' collection doesn't exist, it will be created. Otherwise, it is used.
-        device_collection = db.collection
-        person_collection = db.collection
-        project_collection = db.collection
+        self.project_collection = db.projects

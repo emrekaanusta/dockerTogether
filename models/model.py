235c3181c8ec48cf46b2b5263ae3d6a73e1a7620
gizemico <gizemseval48@gmail.com>
2023-07-17 11:21:01 +0300

    models folder is updated

--- models/model.py
+++ models/model.py
@@ -1,26 +1,19 @@
 class projectService:
     
 
-    def __init__(self):
+    def _init_(self):
         self.name = None
         self.description = None
         self.project = None
     
+class deviceService:
 
-    def read_project(self):
-        for p in self.collection.find():
-            print(p)
-
-    def create_project(self,name, description, project):
-        if name is None or project is None:
-            print("Name and Project cannot be NULL")
-            return __name__()
-        elif description == "":
-            self.db.projects.insert_one({"name": name, "desc": "", "project": project})
-        else:
-            self.db.projects.insert_one({"name": name, "desc": description, "project": project})
-
+    def _init_(self):
+        self.device_number = None
+        self.device_ip = None
+        self.device_port = None
+        self.device_username = None
+        self.device_password = None
 
 projects = projectService()
-projects.create_project("Emre", "Nokia5", "p5")
-projects.read_project()
+devices = deviceService()

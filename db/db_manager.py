bacab9523a15cb330d9f4df2aa9532dff6bcdf18
gizemico <gizemseval48@gmail.com>
2023-07-19 16:08:15 +0300

    db manager is modified

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,34 +1,37 @@
 from pymongo import MongoClient
 
-
-
-class Database:
-
-    def connect_database(self):
+'''
+def connect_database():
         
+    #TODO connect_collection
         
-        '''
-        # connecting MongoDB instance
-        #client = MongoClient("localhost", 27017)
-
-        #  If the 'projectdb' doesn't exist, it is created. Otherwise, it is used.
-        db = client.main_db
-        client = MongoClient("mongodb://localhost:27017/")
-        db.client["projectdb"]
+    
+    # connecting MongoDB instance
+    client = MongoClient("localhost", 27017)
         
-        # If 'project' collection doesn't exist, it will be created. Otherwise, it is used.
-        #device_collection = db.collection
-        #person_collection = db.collection
-        self.project_collection = db["projects"]
-        '''
 
-        # connecting MongoDB instance
+    #  If the 'projectdb' doesn't exist, it is created. Otherwise, it is used.
+    db = client.main_db
+    #client = MongoClient("mongodb://localhost:27017/")
+
+    # If 'project' collection doesn't exist, it will be created. Otherwise, it is used.
+    self.project_collection = db.projects
+
+    #TODO return db[collection_name]
+'''
+
+def connect_collection(name):
+
         client = MongoClient("localhost", 27017)
-        
+        db = client['nokia_project']
 
-        #  If the 'projectdb' doesn't exist, it is created. Otherwise, it is used.
-        db = client.main_db
-        client = MongoClient("mongodb://localhost:27017/")
+        device_collection = db['device_database']
+        project_collection = db['project_collection']
+        person_collection = db['person_collection']
 
-        # If 'project' collection doesn't exist, it will be created. Otherwise, it is used.
-        self.project_collection = db.projects
+        if name == "device":
+            return device_collection
+        if name == "project":
+            return project_collection
+        if name == "person":
+            return person_collection

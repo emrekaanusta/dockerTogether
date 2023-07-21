6f9a1467a9af8bfd24977791747c9a9fcb956877
gizemico <gizemseval48@gmail.com>
2023-07-21 16:17:36 +0300

    db updated

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,24 +1,5 @@
 from pymongo import MongoClient
 
-'''
-def connect_database():
-        
-    #TODO connect_collection
-        
-    
-    # connecting MongoDB instance
-    client = MongoClient("localhost", 27017)
-        
-
-    #  If the 'projectdb' doesn't exist, it is created. Otherwise, it is used.
-    db = client.main_db
-    #client = MongoClient("mongodb://localhost:27017/")
-
-    # If 'project' collection doesn't exist, it will be created. Otherwise, it is used.
-    self.project_collection = db.projects
-
-    #TODO return db[collection_name]
-'''
 
 def connect_collection(name):
 

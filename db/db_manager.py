363fb9ac6e9b227464e5e238063cff8829003cf8
gizemico <gizemseval48@gmail.com>
2023-07-24 16:10:15 +0300

    db_manager is updated for all developers

--- db/db_manager.py
+++ db/db_manager.py
@@ -5,14 +5,6 @@ def connect_collection(name):
 
         client = MongoClient("localhost", 27017)
         db = client['nokia_project']
-
-        device_collection = db['device_database']
-        project_collection = db['project_collection']
-        person_collection = db['person_collection']
-
-        if name == "device":
-            return device_collection
-        if name == "project":
-            return project_collection
-        if name == "person":
-            return person_collection
+        
+        connection_collection = db[name]
+        return connection_collection

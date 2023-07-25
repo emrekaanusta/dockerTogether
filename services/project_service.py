5896f9ae75e4b7545897141b709d42d3fe8320e6
gizemico <gizemseval48@gmail.com>
2023-07-25 10:28:54 +0300

    project_services is modified

--- services/project_service.py
+++ services/project_service.py
@@ -15,27 +15,29 @@ def menu():
           "Enter selection: ")
 
 
-
 #creating new collection
-def create_project(name, description, device):
-    collection.insert_one({"name": name, "description": description, "device": device})
+def create_project(Project):
+    collection.insert_one({"name":Project.name, "description":Project.description, "device":Project.device})
 
 
 #reading available collection from database
 def read_project():
     for p in collection.find():
         print(p)
-#TODO read_all_proj funct, read_project_name/device/..func
+
+
+
 
 #this function enables user to updating project collection
-def update_project(uid, name, description, device):
+def update_project(uid, Project):
     for id in collection.find({"_id": uid}):  # search datas relates selected ObjectId
-        if name != "":
-            collection.update_many({"_id": uid},{"$set": {"name": name}})
-        if description != "":
-            collection.update_one({"_id": uid}, {"$set": {"description": description}})
-        if device != "":
-            collection.update_one({"_id": uid}, {"$set": {"device": device}})
+        if Project.name != "":
+            collection.update_many({"_id": uid},{"$set": {"name": Project.name}})
+        if Project.description != "":
+            collection.update_one({"_id": uid}, {"$set": {"description": Project.description}})
+        if Project.device != "":
+            collection.update_one({"_id": uid}, {"$set": {"device": Project.device}})
+            
 
 #deleting specific collection with using unique project id
 def delete_project(uid):

d2d83abcb48c162a689f7328357d0bce0173b044
gorkemmayadag <gorkemmert.mayadag@bahcesehir.edu.tr>
2023-07-25 16:34:28 +0300

    idk

--- db/db_manager.py
+++ db/db_manager.py
@@ -5,6 +5,4 @@ def connect_collection(name):
     client = MongoClient("localhost", 27017)
     db = client["nokia_project"]
 
-    connection_collection = db[name]
-
-    return connection_collection
+    return db[name]

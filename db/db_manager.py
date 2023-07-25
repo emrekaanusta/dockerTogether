06f5bc904e9c50973674dc87a2d349779d4b1db7
gizemico <gizemseval48@gmail.com>
2023-07-25 10:27:46 +0300

    db_manager is modified

--- db/db_manager.py
+++ db/db_manager.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
 
 
+#connect MongoDB collection with sending collection name parameter
 def connect_collection(name):
 
         client = MongoClient("localhost", 27017)
         db = client['nokia_project']
-        
-        connection_collection = db[name]
-        return connection_collection
+         
+        return db[name]
